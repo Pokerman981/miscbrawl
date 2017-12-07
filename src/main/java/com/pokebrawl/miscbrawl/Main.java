bb6a873923155f0504e1faa4e3ca5d25669cef1a
Pokerman981 <tjg7105@edu.wrsd.ca>
2017-12-06 18:43:22 -0700

    Init

---
+++ src/main/java/com/pokebrawl/miscbrawl/Main.java
@@ -0,0 +1,98 @@
+package com.pokebrawl.miscbrawl;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import org.slf4j.Logger;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.CommandException;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.command.spec.CommandSpec;
+import org.spongepowered.api.config.ConfigDir;
+import org.spongepowered.api.config.DefaultConfig;
+import org.spongepowered.api.event.game.state.GameInitializationEvent;
+import org.spongepowered.api.text.Text;
+
+import com.google.inject.Inject;
+import com.pokebrawl.miscbrawl.events.connectionListener;
+
+import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.loader.ConfigurationLoader;
+
+public class Main {
+
+
+	@Inject
+	@DefaultConfig(sharedRoot = false)
+	private Path defaultConfig;
+
+	@Inject
+	@DefaultConfig(sharedRoot = false)
+	public ConfigurationLoader<CommentedConfigurationNode> loader;
+
+	@Inject
+	@ConfigDir(sharedRoot = false)
+	private Path ConfigDir;
+
+	public static CommentedConfigurationNode config;
+
+	public static CommentedConfigurationNode config() {
+		return config;
+	}
+
+	public void save() throws IOException {
+		loader.save(config);
+	}
+
+	@Inject
+	private Logger logger;
+
+	public Logger getLogger() {
+		return logger;
+	}
+	
+	public void RegisterListeners(GameInitializationEvent e){
+		Sponge.getEventManager().registerListeners(this, new connectionListener(this));
+	}
+	
+	public void registerNodes() throws IOException{
+			config = loader.load();
+			if (!defaultConfig.toFile().exists()) {
+				CommentedConfigurationNode root = config.getNode("messages");
+				root.getNode("join-message").setValue("&1>>> %player% Joined The Server &1<<<").setComment("%player% replaces with players name. Only works for people with perm 'onJoin.bcast'");
+				save();
+			}
+	}
+	
+	public void registerCommand() {
+		CommandSpec reloadCommand = CommandSpec.builder()
+				.description(Text.of("Reload the config"))
+				.permission("miscbrawl.admin").executor(new CommandExecutor() {
+					@Override
+					public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
+						try {
+							config = loader.load();
+							sendMessage(src, "&aReloaded!");} catch (IOException e) {e.printStackTrace();}
+						return CommandResult.success();
+					}
+				})
+				.build();
+	}
+
+	public static String color(String string) {
+		return org.spongepowered.api.text.serializer.TextSerializers.FORMATTING_CODE
+				.serialize(org.spongepowered.api.text.Text.of(string));
+	}
+
+	public static void sendMessage(CommandSource sender, String message) {
+		if (sender == null)
+			return;
+		sender.sendMessage(
+				org.spongepowered.api.text.serializer.TextSerializers.FORMATTING_CODE.deserialize(color(message)));
+	};
+
+	
+}
---
+++ src/main/java/com/pokebrawl/miscbrawl/events/connectionListener.java
@@ -0,0 +1,31 @@
+package com.pokebrawl.miscbrawl.events;
+
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.network.ClientConnectionEvent;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.channel.MessageChannel;
+import org.spongepowered.api.text.serializer.TextParseException;
+import org.spongepowered.api.text.serializer.TextSerializer;
+import org.spongepowered.api.text.serializer.TextSerializers;
+
+import com.pokebrawl.miscbrawl.Main;
+
+public class connectionListener {
+	
+	public Main plugin;
+
+	public connectionListener(Main pluginInstance) {
+		this.plugin = pluginInstance;
+	}
+
+	@Listener
+	public void onJoin(ClientConnectionEvent.Join e){
+		if (e.getTargetEntity().hasPermission("onJoin.bcast")){
+			String message = plugin.config().getNode("messages", "join-message").getString().replace("%player%", e.getTargetEntity().getName());
+			MessageChannel.TO_ALL.send(Text.of(TextSerializers.FORMATTING_CODE.serialize(Text.of(message))));
+		}
+		
+	}
+	
+	
+}
